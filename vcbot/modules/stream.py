c4ba4a9c17a0e751215c1239ffb11e3a88b826aa
DRMECHANIC <73771895+PEARLGANG@users.noreply.github.com>
2021-09-06 16:35:25 +0530

    Update stream.py

--- vcbot/modules/stream.py
+++ vcbot/modules/stream.py
@@ -56,7 +56,7 @@ async def stream_msg_handler(_, m: Message):
             VideoParameters(
                 width=640,
                 height=360,
-                frame_rate=22, 
+                frame_rate=20, 
             ),
         ),
         stream_type=StreamType().pulse_stream,
