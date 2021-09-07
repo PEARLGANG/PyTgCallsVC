714c86dd53aed62090755d2079829871909b5946
DRMECHANIC <73771895+PEARLGANG@users.noreply.github.com>
2021-09-07 15:26:01 +0530

    Update player.py

--- vcbot/modules/player.py
+++ vcbot/modules/player.py
@@ -35,14 +35,12 @@ async def play_msg_handler(_, m: Message):
     if video and (video.file_name.endswith('.mkv') or video.file_name.endswith('.mp4')):
             is_file = True
             link = video
-    if query:
+    else query:
          results = YoutubeSearch(query, max_results=1).to_dict()
          url = f"https://youtube.com{results[0]['url_suffix']}"
          link = re.search(r'((https?:\/\/)?(www\.)?(youtube|youtu|youtube-nocookie)\.(com|be)\/(watch\?v=|embed\/|v\/|.+\?v=)?([^&=%\?]{11}))', url).group(1)
          is_live = await is_ytlive(link)
-    else:
-        await status.edit("Pass me something to Stream!")
-        is_file = False
+         is_file = False
         # todo
     if is_live:
          await m.reply("Error: This is a live link.\nTip: use !stream command.")
